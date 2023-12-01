1f081d2dde4c863d9f114f6ef4f1dd97fe818353
Aloisz <a.tupinon@rubika-edu.com>
2023-12-01 18:08:54 +0100

    Dotween

--- HorrorGameTest/Assets/Script/Object/Simon/Button.cs
+++ HorrorGameTest/Assets/Script/Object/Simon/Button.cs
@@ -4,17 +4,21 @@ using System.Collections.Generic;
 using Player;
 using SimonGame;
 using UnityEngine;
-
+using DG.Tweening;
 public class Button : InteractiveObj
 {
     public SimonState buttonState;
 
+    private Vector3 basePos;
+    [SerializeField] private Vector3 PressedButtonPosition;
     private Collider coll;
     private void Start()
     {
         coll = GetComponent<Collider>();
         coll.enabled = false;
         Simon.instance.OnPlaySimon += ChangeCollider;
+        basePos = transform.localPosition;
+        PressedButtonPosition = basePos + new Vector3(0,-.2f,0);
     }
 
     public override void Interact(PlayerController player)
@@ -22,6 +26,8 @@ public class Button : InteractiveObj
         if (Simon.instance.canPlayTheSimon)
         {
             Simon.instance.PressButton(buttonState);
+            transform.DOMove(PressedButtonPosition, .25f).OnComplete((() => 
+                transform.DOMove(basePos, 0.25f)));
         }
     }
 
