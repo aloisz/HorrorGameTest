3ae2a4116f3b56376e3bfd9830ccae548ba53e0d
aloisz <alois.tupinon@gmail.com>
2023-12-21 16:20:36 +0100

    Nez things

--- HorrorGameTest/Assets/Script/UI/MainMenu.cs
+++ HorrorGameTest/Assets/Script/UI/MainMenu.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private CanvasGroup MainMenuCG;
+
+
     [SerializeField] private UnityEngine.UI.Button playButton;
     [SerializeField] private UnityEngine.UI.Button quitButton;
-
+    
     
     void Start()
     {
+        MainMenuCG.DOFade(0, 0);
+        StartCoroutine(EnableScreen());
+        
         playButton.onClick.AddListener(PlayTheGame);
         quitButton.onClick.AddListener(QuitGame);
+    }
+
+    IEnumerator EnableScreen()
+    {
+        yield return new WaitForSeconds(1.25f);
+        MainMenuCG.DOFade(1, .2f);
     }
 
     // Update is called once per frame
